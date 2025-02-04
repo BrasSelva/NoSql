19f507671a89844ab4e829b3dc641786ab7d8d4b
Brass Selvaratnam <brassica.selvaratnam@gmail.com>
2025-02-04 13:50:30 +0100

    connexionPool

--- test_redis.py
+++ test_redis.py
@@ -1,6 +1,7 @@
 import redis
+from redis import ConnectionPool
+import threading
 
-# Connexion à Redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 r.set('user:1:name', 'John Doe')
@@ -16,8 +17,27 @@ user_email = user_email.decode('utf-8')
 
 keys = ['user:1:name', 'user:1:email']
 values = r.mget(keys)
-# Convert byte values to strings
+
 values = [value.decode('utf-8') for value in values]
+print(f"Values: {values}")
 
-print(f'Name: {user_name}, Email: {user_email}')
+pool = ConnectionPool(host='localhost', port=6379, db=0)
 
+r_custom = redis.Redis(connection_pool=pool)
+
+def worker():
+    r_thread = redis.Redis(connection_pool=pool)
+    r_thread.set('user:2:name', 'Jane Doe')
+    print(f"Thread Redis Operation: {r_thread.get('user:2:name').decode('utf-8')}")
+
+threads = [threading.Thread(target=worker) for _ in range(5)]
+
+# Démarrer tous les threads
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+r_custom.set('user:3:name', 'Alice Smith')
+print(f"User 3 Name: {r_custom.get('user:3:name').decode('utf-8')}")
